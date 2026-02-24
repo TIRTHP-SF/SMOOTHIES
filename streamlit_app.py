d0cfcc843bcfb13d750a3e9c1f3bbd325e4a6523
TIRTHP-SF <golupython94@gmail.com>
2026-02-24 15:06:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingredients_list:
 
     for fruit in ingredients_list:
         ingredients_string+=fruit+' '
-smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
             values ('""" + ingredients_string + """','"""+name_on_order+"""') """
