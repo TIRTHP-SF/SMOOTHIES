00bffe914fadc7c96e81f51ff9f8372cfb5d25e0
TIRTHP-SF <golupython94@gmail.com>
2026-02-24 14:45:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 
 smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
-sf_df=at.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 name_on_order= st.text_input('Your Name')
 st.write("The name on your smoothie is: ", name_on_order)
